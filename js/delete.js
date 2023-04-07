1bd25269fa5cfd955c9c822c7c6b3259789592af
William Harrison <william@williamdavidharrison.com.au>
2023-04-07 10:52:34 +0800

    Create delete.js

---
+++ js/delete.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form");
+const webhook = document.getElementById("webhook");
+const btn = document.getElementById("btn");
+
+async function deleteWebhook() {
+    event.preventDefault();
+
+    if(!webhook.value.startsWith("https://discord.com/api/webhooks/")) return alert("Please enter a valid Discord webhook URL!");
+
+    btn.setAttribute("disabled", true);
+    btn.innerHTML = "Deleting...";
+
+    fetch(webhook.value, {
+        method: "DELETE"
+    }).then(res => {
+        form.reset();
+
+        btn.removeAttribute("disabled");
+        btn.innerHTML = "Delete";
+
+        if(res.status === 204) {
+            alert("Webhook has been deleted!");
+        } else {
+            alert("Webhook does not exist!");
+        }
+    })
+}
